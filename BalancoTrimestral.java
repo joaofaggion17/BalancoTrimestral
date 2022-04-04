c5e98643c9d24d3378afa8d9ef6fcf868ee61a63
joaofaggion17 <joaofaggion17@gmail.com>
2022-04-04 16:59:37 +0000

    Salvando

---
+++ BalancoTrimestral.java
@@ -0,0 +1,11 @@
+public class BalancoTrimestral{
+    public static void main(String[] args) {
+
+        double gastoAbril = 1212.00; //Valor do salário Mínimo
+
+        System.out.printf("Valor do salário Mínimo: %.2f R$",gastoAbril," R$");
+        System.out.printf("\nValor do salário Mínimo multiplicado por 10: %.2f reais", (gastoAbril * 10)," reais  ");
+        
+    }
+
+}
